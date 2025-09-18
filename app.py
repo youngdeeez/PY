be8d0396cbdb8092b95f005cc097a8a9f7f26a93
youngdeeez <daniil201520151@gmail.com>
2025-09-18 03:14:39 +0300

    Пытаемся завести контейнер в продакшен 1

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello_devops():
     return '<h1>Hello, DevOps!</h1><p>This is my first automated deployment.</p>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
