67ebcb46e503d1bbc12b010ec62ea0f9197c999d
youngdeeez <daniil201520151@gmail.com>
2025-09-18 04:00:58 +0300

    add prometheus

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest, REGISTRY
+from prometheus_client.exposition import CONTENT_TYPE_LATEST
+
 app = Flask(__name__)
-#test11
+
+# Создаем метрики
+REQUEST_COUNT = Counter('request_count', 'App Request Count', ['method', 'endpoint'])
+
 @app.route('/')
-def hello_devops():
-    return '<h1>Hello, DevOps!</h1><p>This is my first automated deployment.</p>'
+def hello_world():
+    REQUEST_COUNT.labels(method='GET', endpoint='/').inc()
+    return 'Hello, World!'
+
+@app.route('/metrics')
+def metrics():
+    REQUEST_COUNT.labels(method='GET', endpoint='/metrics').inc()
+    return generate_latest(REGISTRY), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
